8ed2dd176d1774ebdfccb437f3b25698802a5b07
Giang Soda <truonggiang.92.soda@gmail.com>
2023-04-12 15:04:29 +0700

    Create cors.ts

---
+++ src/config/cors.ts
@@ -0,0 +1,45 @@
+import config from './index';
+
+const sitesEnv = {
+  dev: ['http://localhost:3000', '^(https://([^.]*.)?soda.net)$'],
+};
+
+const CORS_SITE = {
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  credentials: true,
+  origin: '*' as any,
+};
+let sites: string[] = [];
+
+if (config.CORS_ORIGINS) {
+  sites = config.CORS_ORIGINS.split(',');
+}
+if (sites.length === 0) {
+  if (config.isProd) {
+    sites = sitesEnv.prod;
+  } else {
+    sites = sitesEnv.dev;
+  }
+}
+const matchRegHost = (sites: string[], host: string): boolean => {
+  for (const site of sites) {
+    if (site === '*') {
+      return true;
+    }
+    const r = new RegExp(site);
+    if (r.test(host)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+CORS_SITE.origin = function (origin: string, callback: any) {
+  if (!origin || sites.includes(origin) || matchRegHost(sites, origin)) {
+    callback(null, true);
+  } else {
+    console.error('CORS', origin);
+    callback(new Error('Not allowed by CORS'));
+  }
+};
+export default CORS_SITE;

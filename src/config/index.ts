2c809b3c22a5b6ea3653e881409210aac9f1b7df
Giang Soda <truonggiang.92.soda@gmail.com>
2023-04-12 15:07:21 +0700

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,42 +1,63 @@
 import { config as dotenvLib } from 'dotenv';
 import * as fs from 'fs';
+const baseDir = __dirname + '/../../';
 
-const pathEnv = [
-  `.env.${process.env.NODE_ENV}`,
-  '.env',
-];
-const baseDir = process.env.PWD
+/**
+ * find .env file: .env.prod, not eixsts -> .env
+ */
 const getPathEnv = () => {
-  for (const path of pathEnv) {
-    if (fs.existsSync(path)) {
-      return path;
-    }
-    if (fs.existsSync(baseDir + path)) {
-      return baseDir + path;
-    }
+  if (fs.existsSync(`.env.${process.env.NODE_ENV}`)) {
+    return `.env.${process.env.NODE_ENV}`;
+  }
+  if (fs.existsSync(baseDir + `.env.${process.env.NODE_ENV}`)) {
+    return baseDir + `.env.${process.env.NODE_ENV}`;
+  }
+  if (fs.existsSync('.env')) {
+    return '.env';
+  }
+  if (fs.existsSync(baseDir + '.env')) {
+    return baseDir + '.env';
   }
   return null;
-}
+};
 const configDotenv: any = {
   path: getPathEnv(),
 };
 dotenvLib(configDotenv);
 if (configDotenv.path) {
-  console.log('++++ Load file env', configDotenv.path);
+  console.info('+_+ =_= ----- Load file env', configDotenv.path);
 }
+const appEnv = process.env.APP_ENV?.toLowerCase();
 
 const configDefault = {
-  basedir: baseDir,
-  appdir: baseDir + 'src/',
-  storagedir: baseDir + 'storage/',
-  isProd: process.env.APP_ENV &&
-    (process.env.APP_ENV.toLowerCase() === 'prod' ||
-      process.env.APP_ENV.toLowerCase() === 'production')
-    ? true : false,
-    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-    token_uri: 'https://oauth2.googleapis.com/token',
-    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs'
+  basedir: __dirname + '/../../',
+  isProd: ['prod', 'production'].includes(appEnv) ? true : false,
+  isRunCmd: process.env.RUN_CMD === '1',
+  RPC_PROTOCOL: 'https://',
 };
 
-const config: any = Object.assign(configDefault, process.env);
+const config = Object.assign(configDefault, process.env);
+if (!config.RPC_URL) {
+  config.RPC_URL = config.RPC_PROTOCOL + config.RPC_HOST;
+}
+
+const redisOption: any = {};
+redisOption.host = config.REDIS_HOST;
+redisOption.port = Number(config.REDIS_PORT);
+if (config.REDIS_USERNAME) {
+  redisOption.username = config.REDIS_USERNAME;
+}
+if (config.REDIS_PASSWORD) {
+  redisOption.password = encodeURIComponent(config.REDIS_PASSWORD);
+}
+if (config.REDIS_DB) {
+  redisOption.db = Number(config.REDIS_DB);
+} else {
+  redisOption.db = 0;
+}
+redisOption.url = `redis://${redisOption.username ?? ''}:${
+  redisOption.password ?? ''
+}@${redisOption.host}:${redisOption.port}/${redisOption.db}`;
+
 export default config;
+export { redisOption };
